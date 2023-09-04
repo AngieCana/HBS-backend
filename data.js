29482ba177b5c80016a0552301a420a8870c6d22
AngieCana <grilozangie@gmail.com>
2023-09-04 19:15:52 -0400

    re-structered schema for user.

--- data.js
+++ data.js
@@ -1,7 +1,27 @@
+import bcrypt from 'bcrypt'
+const saltRounds = 10;
+const pass1 = '123456';
+
 const data = {
   //creating the top 8 products that will be shown on the main page when the user first visits the site
+  //added _id so that each product would have unique identifier
+  // users: [
+  //   {
+  //     name: 'Angie',
+  //     email: 'admin@example.com',
+  //     password: bcrypt.hashSync('123456', saltRounds),
+  //     isAdmin: true
+  //   },
+  //   {
+  //     name: 'Jane',
+  //     email: 'user@example.com',
+  //     password: bcrypt.hashSync('123456', saltRounds),
+  //     isAdmin: false, 
+  //   }
+  // ],
   products: [
     {
+      // _id: '1',
       name: 'Albedo Bushiroad Sleeves - Overlord IV Vol. 3522',
       slug: 'albedo-sleeves-3522',
       category: 'card sleeve',
@@ -12,6 +32,7 @@ const data = {
       description: 'Albedo Bushiroad High Grade Sleeves - Overlord IV Vol. 3522. 75ct Sleeves , standard size (weiss, pokemon, Magic the gathering, One Piece)'
     },
     {
+      // _id: '2',
       name: 'Shalltear Bushiroad Sleeves - Overlord IV Vol.3523',
       slug: 'shalltear-sleeves-3523',
       category: 'card sleeve',
@@ -22,6 +43,7 @@ const data = {
       description: 'Shalltear Bushiroad Card Sleeve Collection High Grade Vol.3523 Overlord IV. 75ct Sleeves , standard size (weiss, pokemon, Magic the gathering, One piece)'
     },
     {
+      // _id: '3',
       name: 'Albedo Bushiroad Rubber Mat Collection V2 Vol.595',
       slug: 'albedo-playmat-595',
       category: 'playmat',
@@ -32,6 +54,7 @@ const data = {
       description: 'Albedo Bushiroad Rubber Mat Collection V2 Vol.595 Overlord IV ED Ver. Playmat.'
     },
     {
+      // _id: '4',
       name: 'Overlord IV Key Visual Bushiroad Sleeves Vol.3520',
       slug: 'overlord-sleeves-3520',
       category: 'card sleeve',
@@ -42,6 +65,7 @@ const data = {
       description: 'Overlord IV Key Visual Bushiroad Sleeve Collection High Grade Vol.3520.'
     },
     {
+      // _id: '5',
       name: 'Albedo Pt. 2 Overlord IV Bushiroad Sleeves Vol.3524 ',
       slug: 'albedo-sleeves-3524',
       category: 'card sleeve',
@@ -52,6 +76,7 @@ const data = {
       description: 'Albedo Pt. 2 Overlord IV Bushiroad Sleeve Collection High Grade Vol.3524'
     },
     {
+      // _id: '6',
       name: 'Ainz Key Visual Bushiroad Rubber Mat Collection V2 Vol.593',
       slug: 'ainz-playmat-593',
       category: 'playmat',
@@ -62,6 +87,7 @@ const data = {
       description: 'Ainz Bushiroad Rubber Mat Collection V2 Vol.594 Overlord IV ED Ver. Playmat.'
     },
     {
+      // _id: '7',
       name: 'Key Visual Bushiroad Rubber Mat Collection V2 Vol.592',
       slug: 'overlord-playmat-592',
       category: 'playmat',
@@ -72,6 +98,7 @@ const data = {
       description: 'KEY visual z Bushiroad Rubber Mat Collection V2 Vol.592 Overlord IV ED Ver. Playmat.'
     },
     {
+      // _id: '8',
       name: 'High School DxD Born Akeno Card Sleeve Mat Series No.MT212',
       slug: 'akeno-sleeves-mt212',
       category: 'card sleeve',
--- server.js
+++ server.js
@@ -1,28 +1,89 @@
 import express from "express";
 import data from "./data.js";
-import 'dotenv/config'
+import "dotenv/config";
+import mongoose from "mongoose";
+import seedRouter from "./routes/seedRoute.js";
+import productRouter from "./routes/productRoutes.js";
+import bodyparser from "body-parser";
+import userRouter from "./routes/userRoutes.js";
+import expressAsyncHandler from 'express-async-handler';
+import cors from "cors";
+import User from "./models/userModel.js";
+import axios from "axios"
+
 
 //changed module type to es6 so import and export can be used instead of require
 
 const app = express();
 
-//path that will be accessing our api. when you type the filepath it will show you the list of the top 8 products
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
+
+//code for sign in 
+app.post('/signin', async(req, res) => {
+  const {email, password} = req.body;
+  try {
+      const check = await User.findOne({email: email})
+      if (check){
+        res.json("exists")
+        console.log('email exists')
+      } else {
+        res.json("Email does not exist")
+      }
+  } catch (err) {
+    console.log(e)
+    res.json("Does not exist")
+  }
 })
 
-//will get the slug that user entered in the url
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
+//code for sign up
+app.post('/signup', async(req, res) => {
+  const {email, password} = req.body;
+  const data = {
+    email: email,
+    password: password
   }
+  try {
+      const check = await User.findOne({email: email})
+      if (check){
+        res.json("exist")
+        console.log('email exists')
+      } else {
+        res.json("notexist")
+        await User.insertMany([data])
+      }
+  } catch (err) {
+    console.log(err);
+    res.json("Does not exist")
+  }
+})
+
+//form data in post request will be converted into JSON 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+
+//connect to mongoose
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to MongoDB.");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+//define error handler for sign in api. accepts 4 parameters
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 //define the port that will be responding from the backend. if the first port does not work, it will default to 5000.
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
